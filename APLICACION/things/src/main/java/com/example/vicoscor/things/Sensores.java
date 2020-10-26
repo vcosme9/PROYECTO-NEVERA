a2390431143c148e051a34c75aff3e7a2aaf2a01
David09x <71710020+David09x@users.noreply.github.com>
2020-10-26 19:21:06 +0100

    Create Sensores.java

---
+++ APLICACION/things/src/main/java/com/example/vicoscor/things/Sensores.java
@@ -0,0 +1,53 @@
+package com.example.vicoscor.things;
+
+import android.app.Activity;
+
+public class Sensores  extends Activity {
+
+    private String Magnetico;
+    private String id;
+    private double humedad;
+    private double temperatura;
+
+    public Sensores(String magnetico, String id) {
+        Magnetico = magnetico;
+        this.id = id;
+    }
+
+    public Sensores(double humedad, double temperatura) {
+        this.humedad = humedad;
+        this.temperatura = temperatura;
+    }
+
+    public String getMagnetico() {
+        return Magnetico;
+    }
+
+    public void setMagnetico(String magnetico) {
+        Magnetico = magnetico;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getHumedad() {
+        return humedad;
+    }
+
+    public void setHumedad(double humedad) {
+        this.humedad = humedad;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+}
