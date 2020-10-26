770a7b734f3042eaa1c40e11803ceec347cd8576
David09x <71710020+David09x@users.noreply.github.com>
2020-10-26 20:34:32 +0100

    cambios en sensores
    cambiamos lo que le entra

--- APLICACION/things/src/main/java/com/example/vicoscor/things/Sensores.java
+++ APLICACION/things/src/main/java/com/example/vicoscor/things/Sensores.java
@@ -2,53 +2,39 @@ package com.example.vicoscor.things;
 
 import android.app.Activity;
 
-public class Sensores  extends Activity {
+public class Sensores {
 
-    private String Magnetico;
-    private String id;
-    private double humedad;
-    private double temperatura;
-    private String camara;
+    private String fecha;
+    private String palabras;
+    private double numeros;
 
-    public Sensores(String magnetico, String id) {
-        Magnetico = magnetico;
-        this.id = id;
+
+    public Sensores(String fecha, String palabras) {
+        this.fecha = fecha;
+        this.palabras = palabras;
     }
 
-    public Sensores(double humedad, double temperatura) {
-        this.humedad = humedad;
-        this.temperatura = temperatura;
+    public String getFecha() {
+        return fecha;
     }
 
-    public String getMagnetico() {
-        return Magnetico;
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
     }
 
-    public void setMagnetico(String magnetico) {
-        Magnetico = magnetico;
+    public String getPalabras() {
+        return palabras;
     }
 
-    public String getId() {
-        return id;
+    public void setPalabras(String palabras) {
+        this.palabras = palabras;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public double getNumeros() {
+        return numeros;
     }
 
-    public double getHumedad() {
-        return humedad;
-    }
-
-    public void setHumedad(double humedad) {
-        this.humedad = humedad;
-    }
-
-    public double getTemperatura() {
-        return temperatura;
-    }
-
-    public void setTemperatura(double temperatura) {
-        this.temperatura = temperatura;
+    public void setNumeros(double numeros) {
+        this.numeros = numeros;
     }
 }
